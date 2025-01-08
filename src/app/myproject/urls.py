ec9d6484e5334056fa9814cc7c157a2c90929d15
Louis Touzalin <louistouz@icloud.com>
2025-01-08 17:57:22 +0100

    CSS de profile

--- src/app/myproject/urls.py
+++ src/app/myproject/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     path('api/update-profile', views.update_profile, name='update_profile'),
     path('api/change-password', views.change_password, name='change_password'),
     path('api/save-profile', views.save_profile, name='save_profile'),
-
 ]
