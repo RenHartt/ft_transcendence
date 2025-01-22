5ddcb70b5fc9bb4c284f91f4620143b0df59cfab
Louis Touzalin <louistouz@icloud.com>
2025-01-22 17:12:56 +0100

    nginx + entrypoint with dynamic links for static files

--- src/app/nginx_entry.sh
+++ src/app/nginx_entry.sh
@@ -2,4 +2,7 @@
 echo "Création du lien symbolique..."
 ln -nfs /app/myproject/static /usr/share/nginx/html/
 
+chown -R www-data:www-data /app/myproject/static
+chmod -R 755 /app/myproject/static
+
 exec nginx -g "daemon off;"
