21518ce16d6f40065b826e10c6e8fcbc7d443aa4
Baptiste Goron <bgoron@2F4.42angouleme.fr>
2025-01-23 15:23:58 +0100

    test nginx

--- src/app/nginx_entry.sh
+++ src/app/nginx_entry.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 echo "Création du lien symbolique..."
-ln -nfs /app/myproject/static /usr/share/nginx/html/
+ln -ds /app/myproject/static /usr/share/nginx/html/
 
-chown -R www-data:www-data /app/myproject/static
+# sed -i 's/^user .*/user root;/' /etc/nginx/nginx.conf
+
+# chown -R nginx:nginx /app/myproject/static
 chmod -R 755 /app/myproject/static
+
+cp ./app/nginx.conf /etc/nginx/nginx.conf
+cp ./app/default.conf /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
